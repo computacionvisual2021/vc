48a2794ad9055a85479ece4fb28726c6df7aec97
Miguel Medellín <mamedellinr@slabcode.com>
2021-03-09 11:09:29 -0500

    update

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -1,34 +1,34 @@
-import { getRenderer } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
-import { initJssCs } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
-import { installTheme } from '/home/runner/work/vc/vc/.codedoc/content/theme.ts';installTheme();
-import { zoomOnFormula } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/formula/zoom-on-formula.js';zoomOnFormula();
-import { codeSelection } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
-import { sameLineLengthInCodes } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
-import { initHintBox } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
-import { initCodeLineRef } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
-import { initSmartCopy } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
-import { copyHeadings } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
-import { contentNavHighlight } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
-import { loadDeferredIFrames } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
-import { smoothLoading } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
-import { tocHighlight } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
-import { postNavSearch } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
-import { copyLineLinks } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
-import { gatherFootnotes } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
-import { ToCPrevNext } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
-import { CollapseControl } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
-import { GithubSearch } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
-import { ToCToggle } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
-import { DarkModeSwitch } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
-import { ConfigTransport } from '/home/runner/work/vc/vc/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { getRenderer } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/transport/renderer.js';
+import { initJssCs } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/transport/setup-jss.js';initJssCs();
+import { installTheme } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/content/theme.ts';installTheme();
+import { zoomOnFormula } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/formula/zoom-on-formula.js';zoomOnFormula();
+import { codeSelection } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { ToCPrevNext } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/prevnext/index.js';
+import { CollapseControl } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/collapse/collapse-control.js';
+import { GithubSearch } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/misc/github/search.js';
+import { ToCToggle } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/components/darkmode/index.js';
+import { ConfigTransport } from 'D:/Backup/Documentos/Universidad Nacional de Colombia/Materias y Electivas/Semestre XIII/Computación Visual/vc/.codedoc/node_modules/@codedoc/core/dist/es5/transport/config.js';
 
 const components = {
-  '7H5M9Vp+I8cVV99ELLvnzg==': ToCPrevNext,
-  'lqlJyab6eRMclBlk8Ftb8g==': CollapseControl,
-  'MChpnTvN8BpuKyiMGzmSYw==': GithubSearch,
-  'X5W7He+ge/Kg3B6xB+T5gg==': ToCToggle,
-  '3ehN2Qj7wHC4donAKEDe0Q==': DarkModeSwitch,
-  'bIAidu6MCPVPBcGGWLCR6Q==': ConfigTransport
+  'jp2IbUSu8C09L45zTbAK7A==': ToCPrevNext,
+  '5uBdbABv/eTEOr7/+xE44Q==': CollapseControl,
+  'y1+4ju67jABlGhX1rjwxcA==': GithubSearch,
+  'I6nsya7X/XZp8FdVx16UAQ==': ToCToggle,
+  'vMI7dNvOSKDwpBPtbCalaQ==': DarkModeSwitch,
+  'A2cF9F9WdUPQORPVNqv9QA==': ConfigTransport
 };
 
 const renderer = getRenderer();
