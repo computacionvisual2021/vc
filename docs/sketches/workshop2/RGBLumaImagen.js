cfd24d2e2ea3527f23d7419c453bc1e82c6037f0
diannyguarin <dcguarina@unal.edu.co>
2021-07-13 06:47:34 +0000

    Deploying to gh-pages from  @ e9514c679c57d96d75810c01ef80ad6d8aee4a55 🚀

--- docs/sketches/workshop2/RGBLumaImagen.js
+++ docs/sketches/workshop2/RGBLumaImagen.js
@@ -1,18 +1,98 @@
-var img;
+let theShader;
+let shaderTexture;
+let img;
+let cam;
+
+let theShaderVideo;
+let shaderVideo;
+let video;
+
+let angle = 0;
+let gray = 0;
+
 function preload() {
-    img = loadImage('/vc/docs/img/cat.jpg');
+	img = loadImage('/vc/docs/img/cat.jpg');
+	video = createVideo('');
+	video.hide();
+	// Cargar los shaders
+	theShader = loadShader('/vc/docs/sketches/workshop2/texture.vert', '/vc/docs/sketches/workshop2/texture.frag');
+	theShaderVideo = loadShader('/vc/docs/sketches/workshop2/texture.vert', '/vc/docs/sketches/workshop2/texture.frag');
+
 }
 
 function setup() {
-    createCanvas(600, 600, WEBGL);
+	pixelDensity(1);
+
+	// Se requiere trabajar con WEBGL
+	canvas = createCanvas(1000, 400, WEBGL);
+	canvas.parent();
+	noStroke();
+
+
+	shaderTexture = createGraphics(512, 512, WEBGL);
+	shaderVideo = createGraphics(windowWidth, windowHeight, WEBGL);
+
+	// Quitar bordes en el createGraphics
+	shaderTexture.noStroke();
+	shaderVideo.noStroke();
+
+	//cam.hide();
+	video.loop();
 }
 
 function draw() {
-    background(220);
+	// Se pasa el shader a la capa del createGraphics
+	shaderTexture.shader(theShader);
 
-    push();
-    translate(-100, -100);
-    texture(img);
-    plane(img.width, img.height);
-    pop();
+
+	// Valores uniform para el fragment shader
+	theShader.setUniform("u_img", img);
+	theShader.setUniform("u_key", gray);
+
+
+	// Renderizar el shader
+	shaderTexture.rect(0, 0, width, height);
+
+	pointLight(255, 255, 255, 0, 0, 500);
+	// Efecto linterna
+	let dx = mouseX - width / 2;
+	let dy = mouseY - height / 2;
+	pointLight(100, 250, 255, dx, dy, 100);
+
+	background(0);
+
+	push();
+	//Se pasa el shader como textura
+	texture(shaderTexture);
+	translate(200, 0, 0);
+	rotateZ(angle);
+	rotateX(angle);
+	rotateY(angle * 2);
+	box(200);
+	pop();
+
+	// Rotacion de la caja
+	angle += 0.002;
+
+	// Se pasa la imagen original como textura
+	texture(img);
+	ellipse(-250, 0, 350, 350, 100);
+	push();
+
+
+}
+
+// Se ejecuta cuando se presiona cualquier tecla
+function keyPressed() {
+	if (key === '0') {
+		gray = 0;
+	} else if (key === '1') {
+		gray = 1;
+	} else if (key === '2') {
+		gray = 2;
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
