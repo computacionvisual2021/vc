dae50877ed570da3b193a83bc530751141541110
diannyguarin <dcguarina@unal.edu.co>
2021-07-26 07:56:43 +0000

    Deploying to gh-pages from  @ 2e899f6c576f5dec2424a1fb30588b0a7f4e7138 🚀

--- docs/sketches/workshop2/PerformanceHardGrayScale.js
+++ docs/sketches/workshop2/PerformanceHardGrayScale.js
@@ -4,6 +4,14 @@ let theShader;
 // this variable will hold our webcam video
 let cam;
 let gray = 0;
+
+//MEDICION FPS
+var fpsVal; //FPS actuales
+var framesTotalVal; //Total de frames ejecutados
+var deltaTimeVal; // Tiempo entre ejecucion de frames
+var FpsPromVal; // Promedio de fps en lo ejecutado hasta el momento
+let fpsTotal = 0.0; // Acumulador de todos los fps para el calculo del promedio
+
 function preload() {
     // load the shader
     theShader = loadShader('/vc/docs/sketches/workshop2/webcam.vert', '/vc/docs/sketches/workshop2/webcam.frag');
@@ -22,6 +30,42 @@ function setup() {
     //Esconde la captura para solo mostrar el renderizado final
     cam.hide();
     frameRate(100);
+
+    // MEDICION FPS
+    // Texto de Frames actuales
+    let fpsTxt = createP("FPS actual: ");
+    fpsTxt.position(50, 400);
+    fpsTxt.style('font-size', '30px');
+    //Valor cambiante de los fps actuales
+    fpsVal = createDiv(0);
+    fpsVal.position(190, 430);
+    fpsVal.style('font-size', '30px');
+    //Texto de Cantidad de frames totales
+    let framesTotalTxt = createP("Cantidad frames totales: ");
+    framesTotalTxt.position(50, 450);
+    framesTotalTxt.style('font-size', '30px');
+    //Valor cambiante de Cantidad de frames totales
+    framesTotalVal = createDiv(0);
+    framesTotalVal.position(350, 480);
+    framesTotalVal.style('font-size', '30px');
+    //Texto de tiempo entre frames
+    let deltaTimeTxt = createP("Tiempo (ms) entre el frame anterior y el actual: ");
+    deltaTimeTxt.position(50, 500);
+    deltaTimeTxt.style('font-size', '30px');
+    //Valor cambiante de tiempo entre frames
+    deltaTimeVal = createDiv(0);
+    deltaTimeVal.position(190, 560);
+    deltaTimeVal.style('font-size', '30px');
+    //Texto de fps promedio
+    let FpsPromTxt = createP("FPS Promedio: ");
+    FpsPromTxt.position(50, 570);
+    FpsPromTxt.style('font-size', '30px');
+    //Valor cambiante de fps promedio
+    FpsPromVal = createDiv(0);
+    FpsPromVal.position(250, 600);
+    FpsPromVal.style('font-size', '30px');
+
+    
 }
 
 function draw() {
@@ -35,13 +79,13 @@ function draw() {
     // rect gives us some geometry on the screen
     rect(0, 0, width, height);
 
-    //Imprime FRAMERATE en consola del navegador (webgl no muestra esto de manera sencilla)
-    print("FPS: " + frameRate());
-    print("Cantidad Frames: " + frameCount);
-    print("Tiempo entre Frames: " + deltaTime);
-    //textSize(30);
-    //fill(255, 255, 255);
-    //text("FPS: " + frameRate(), 100, 300);
+    //Muestra valores de rendimiento en pantalla
+    fpsVal.html(frameRate()); //fps
+    framesTotalVal.html(frameCount);//Cantidad de frames
+    deltaTimeVal.html(deltaTime);// Tiempo entre frames
+    fpsTotal += frameRate();//Se acumula el valor de fps para calcular el promedio
+    FpsPromVal.html(fpsTotal / frameCount);//Promedio de fps
+    
 }
 // Se ejecuta cuando se presiona cualquier tecla
 function keyPressed() {
--- docs/sketches/workshop2/PerformanceHardMascarasVideo.js
+++ docs/sketches/workshop2/PerformanceHardMascarasVideo.js
@@ -4,24 +4,66 @@ let videoPath = "/vc/docs/img/mylivewallpapers.com-Fire-N-Water-Wolves.mp4";
 let theShader;
 let cam;
 let filter = 0;
+
+//MEDICION FPS
+var fpsVal; //FPS actuales
+var framesTotalVal; //Total de frames ejecutados
+var deltaTimeVal; // Tiempo entre ejecucion de frames
+var FpsPromVal; // Promedio de fps en lo ejecutado hasta el momento
+let fpsTotal = 0.0; // Acumulador de todos los fps para el calculo del promedio
+
 function preload() {
     // precargar el shader
     theShader = loadShader('/vc/docs/sketches/workshop2/webcam.vert', '/vc/docs/sketches/workshop2/webcamMask.frag');
 }
 
 function setup() {
-    //Crear canbas de tipo webgl
-    createCanvas(710, 600, WEBGL);
+    //Crear canvas de tipo webgl
+    createCanvas(710, 400, WEBGL);
     noStroke();
     //Crea una captura de video instantanea 
     //cam = createCapture(VIDEO);
     
     cam = createVideo(['/vc/docs/img/mandrill.webm']);
-    cam.size(100, 100);
+    
     cam.loop();
     //Esconde la captura para solo mostrar el renderizado final
     cam.hide();
     frameRate(100);
+
+    // MEDICION FPS
+    // Texto de Frames actuales
+    let fpsTxt = createP("FPS actual: ");
+    fpsTxt.position(50, 400);
+    fpsTxt.style('font-size', '30px');
+    //Valor cambiante de los fps actuales
+    fpsVal = createDiv(0);
+    fpsVal.position(190, 430);
+    fpsVal.style('font-size', '30px');
+    //Texto de Cantidad de frames totales
+    let framesTotalTxt = createP("Cantidad frames totales: ");
+    framesTotalTxt.position(50, 450);
+    framesTotalTxt.style('font-size', '30px');
+    //Valor cambiante de Cantidad de frames totales
+    framesTotalVal = createDiv(0);
+    framesTotalVal.position(350, 480);
+    framesTotalVal.style('font-size', '30px');
+    //Texto de tiempo entre frames
+    let deltaTimeTxt = createP("Tiempo (ms) entre el frame anterior y el actual: ");
+    deltaTimeTxt.position(50, 500);
+    deltaTimeTxt.style('font-size', '30px');
+    //Valor cambiante de tiempo entre frames
+    deltaTimeVal = createDiv(0);
+    deltaTimeVal.position(190, 560);
+    deltaTimeVal.style('font-size', '30px');
+    //Texto de fps promedio
+    let FpsPromTxt = createP("FPS Promedio: ");
+    FpsPromTxt.position(50, 570);
+    FpsPromTxt.style('font-size', '30px');
+    //Valor cambiante de fps promedio
+    FpsPromVal = createDiv(0);
+    FpsPromVal.position(250, 600);
+    FpsPromVal.style('font-size', '30px');
 }
 
 function draw() {
@@ -37,13 +79,13 @@ function draw() {
     theShader.setUniform('dist', 3.0);
 
     rect(0, 0, 710, 400);
-    //Imprime FRAMERATE en consola del navegador (webgl no muestra esto de manera sencilla)
-    print("FPS: " + frameRate());
-    print("Cantidad Frames: " + frameCount);
-    print("Tiempo entre Frames: " + deltaTime);
-    //textSize(30);
-    //fill(255, 255, 255);
-    //text("FPS: " + frameRate(), 100, 300);
+
+    //Muestra valores de rendimiento en pantalla
+    fpsVal.html(frameRate()); //fps
+    framesTotalVal.html(frameCount);//Cantidad de frames
+    deltaTimeVal.html(deltaTime);// Tiempo entre frames
+    fpsTotal += frameRate();//Se acumula el valor de fps para calcular el promedio
+    FpsPromVal.html(fpsTotal / frameCount);//Promedio de fps
 }
 // Se ejecuta cuando se presiona alguno de los numeros
 function keyPressed() {
