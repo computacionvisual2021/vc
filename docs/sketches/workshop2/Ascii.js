3d6643a4f1a342738e23ab87bf4c3d956633f0c6
lyarevalo <50886665+lyarevalo@users.noreply.github.com>
2021-07-26 17:49:14 +0000

    Deploying to gh-pages from  @ 0fd944e20b0e514463e62fea611af8852111337a 🚀

--- docs/sketches/workshop2/Ascii.js
+++ docs/sketches/workshop2/Ascii.js
@@ -1,22 +1,29 @@
-let asciiShader;
+let theShader;
 let shaderTexture;
 
+// Esta variable contrendrá la imagen
+let img;
 
 function preload() {
+  // Precargar la imagen y  el  shader
   img = loadImage('/vc/docs/img/cat.jpg');
-  asciiShader = loadShader('/vc/docs/sketches/workshop2/texturaAscii.vert', '/vc/docs/sketches/workshop2/texturaAscii.frag');
+  theShader = loadShader('/vc/docs/sketches/workshop2/texturaAscii.vert', '/vc/docs/sketches/workshop2/texturaAscii.frag');
 }
 
 function setup() {
+  // shaders require WEBGL mode to work
   createCanvas(768, 500, WEBGL);
   shaderTexture = createGraphics(700, 800, WEBGL);
   shaderTexture.noStroke();
 }
 
 function draw() {
-  shaderTexture.shader(asciiShader);
-  asciiShader.setUniform('tex', img);
+  // shader() Activación del Shader con nuestro Shader
+  shaderTexture.shader(theShader);
+  // Se pasa la imagen como textura
+  theShader.setUniform('tex', img);
   texture(shaderTexture);
+  // rect gives us some geometry on the screen
   shaderTexture.rect(0,0,500,500);
   rect(-500,-500/2.0,500,500)
 }
